f166629432dde964a048cc31e9a40d391c0f2057
zambon03 <luiz_gustavo_zambon@hotmail.com>
2016-03-12 14:15:25 -0300

    Versão Final
    Teste de upload de aula

---
+++ login/cadastro.php
@@ -0,0 +1,36 @@
+﻿<?php
+	$login = $_POST['login'];
+	$senha = MD5($_POST['senha']);
+	$connect = mysql_connect('localhost','root','');
+	$db = mysql_select_db('sistema_login');
+	$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+	$select = mysql_query($query_select,$connect);
+	$arrya = $array['login'];
+	$logoarray = $array['login'];
+	if($login == "" || $login == null)
+	{
+		echo"<script language='javascript' type='text/javascript>alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
+	}
+	else
+	{
+		if($logarray == $login)
+		{
+			echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='cadastro.html';</script>";
+			die();
+		}
+		else
+		{
+			$query = "INSERT INTO usuarios (login, senha) Values ('$login','senha')";
+			$insert = mysql_query($query,$connect);
+		
+			if ($insert)
+			{
+				echo "<script language= 'javascript' type= 'text/javascript'>alert(Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
+			}
+			else
+			{
+				echo "<script language= 'javascript' type= 'text/javascript'>alert(Não foi possível cadastrar usuátio');window.location.href='cadastro.html'</script>";
+			}
+		}
+	}
+?>
